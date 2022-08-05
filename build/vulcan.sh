723a653a47742f899a57db5316a8bf19fb3c9e7e
Martynas Sklizmantas <saint@ghost.lt>
2022-08-05 21:21:21 +0300

    build for php56

--- build/vulcan.sh
+++ build/vulcan.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
 set -e
 
+mkdir -p /app
 mkdir /app/local
 mkdir /app/local/lib
 mkdir /app/local/bin
@@ -9,144 +10,143 @@ mkdir /app/apache
 mkdir /app/php
 
 cd /tmp
-curl -O http://mirrors.us.kernel.org/ubuntu/pool/universe/m/mcrypt/mcrypt_2.6.8-1_amd64.deb
-curl -O http://mirrors.us.kernel.org/ubuntu/pool/universe/libm/libmcrypt/libmcrypt4_2.5.8-3.1_amd64.deb
-curl -O http://mirrors.us.kernel.org/ubuntu/pool/universe/libm/libmcrypt/libmcrypt-dev_2.5.8-3.1_amd64.deb
+curl -O https://mirrors.edge.kernel.org/ubuntu/pool/universe/m/mcrypt/mcrypt_2.6.8-6_amd64.deb
+curl -O https://mirrors.edge.kernel.org/ubuntu/pool/universe/libm/libmcrypt/libmcrypt4_2.5.8-7_amd64.deb
+curl -O https://mirrors.edge.kernel.org/ubuntu/pool/universe/libm/libmcrypt/libmcrypt-dev_2.5.8-7_amd64.deb
 ls -tr *.deb > packages.txt
 while read l; do
     ar x $l
-    tar -xzf data.tar.gz
-    rm data.tar.gz
+    file data.tar.zst
+    tar xvf data.tar.zst
+    rm data.tar.zst
 done < packages.txt
 
 cp -a /tmp/usr/include/* /app/local/include
 cp -a /tmp/usr/lib/* /app/local/lib
+cp -a /tmp/usr/lib/x86_64-linux-gnu/* /app/local/lib/
 
-export APACHE_MIRROR_HOST="http://www.apache.org/dist"
-# export APACHE_MIRROR_HOST="http://apache.mirrors.tds.net"
+export APACHE_MIRROR_HOST="https://downloads.apache.org/"
+export HTTPD_VERSION=2.4.54
+export APR_VERSION=1.7.0
+export APR_UTIL_VERSION=1.6.1
+export ZLIB_VERSION=1.2.12
+export PHP_VERSION=5.6.40
+export PCRE_VERSION=8.45
+export EXPAT_VERSION=2.4.8
+export LIBXML2_VERSION=2.9.14
+export OPENSSL_VERSION=1_1_1q
+export CURL_VERSION=7.84.0
 
-# curl -L ftp://mcrypt.hellug.gr/pub/crypto/mcrypt/libmcrypt/libmcrypt-2.5.7.tar.gz -o /tmp/libmcrypt-2.5.7.tar.gz
-# curl -L ftp://ftp.andrew.cmu.edu/pub/cyrus-mail/cyrus-sasl-2.1.25.tar.gz -o /tmp/cyrus-sasl-2.1.25.tar.gz
-echo "downloading libmemcached"
-curl -L https://launchpad.net/libmemcached/1.0/1.0.16/+download/libmemcached-1.0.16.tar.gz -o /tmp/libmemcached-1.0.16.tar.gz
-echo "downloading PCRE"
-curl -L ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.34.tar.gz -o /tmp/pcre-8.34.tar.gz
+
+echo "downloading pcre"
+curl -L https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz/download -o /tmp/pcre-${PCRE_VERSION}.tar.gz
 echo "downloading apr"
-curl -L ${APACHE_MIRROR_HOST}/apr/apr-1.5.0.tar.gz -o /tmp/apr-1.5.0.tar.gz
+curl -L ${APACHE_MIRROR_HOST}/apr/apr-${APR_VERSION}.tar.gz -o /tmp/apr-${APR_VERSION}.tar.gz
 echo "downloading apr-util"
-curl -L ${APACHE_MIRROR_HOST}/apr/apr-util-1.5.3.tar.gz -o /tmp/apr-util-1.5.3.tar.gz
+curl -L ${APACHE_MIRROR_HOST}/apr/apr-util-${APR_UTIL_VERSION}.tar.gz -o /tmp/apr-util-${APR_UTIL_VERSION}.tar.gz
 echo "downloading httpd"
-curl -L ${APACHE_MIRROR_HOST}/httpd/httpd-2.4.9.tar.gz -o /tmp/httpd-2.4.9.tar.gz
+curl -L ${APACHE_MIRROR_HOST}/httpd/httpd-${HTTPD_VERSION}.tar.gz -o /tmp/httpd-${HTTPD_VERSION}.tar.gz
 echo "downloading php"
-curl -L http://us.php.net/get/php-5.5.11.tar.gz/from/us2.php.net/mirror -o /tmp/php-5.5.11.tar.gz
-echo "downloading pecl-memcached"
-curl -L http://pecl.php.net/get/memcached-2.1.0.tgz -o /tmp/memcached-2.1.0.tgz
+curl -L http://us.php.net/get/php-${PHP_VERSION}.tar.gz/from/us2.php.net/mirror -o /tmp/php-${PHP_VERSION}.tar.gz
 echo "download zlib"
-curl -L http://zlib.net/zlib-1.2.8.tar.gz -o /tmp/zlib-1.2.8.tar.gz
-# echo "downloading pecl zip extension"
-# curl -L http://pecl.php.net/get/zip-1.10.2.tgz -o /tmp/zip-1.10.2.tgz
+curl -L http://zlib.net/zlib-${ZLIB_VERSION}.tar.gz -o /tmp/zlib-${ZLIB_VERSION}.tar.gz
+echo "downloading expat"
+curl -L https://github.com/libexpat/libexpat/releases/download/R_2_4_8/expat-${EXPAT_VERSION}.tar.gz -o /tmp/expat-${EXPAT_VERSION}.tar.gz 
+echo "downloading libxml2"
+curl -L https://download.gnome.org/sources/libxml2/2.9/libxml2-${LIBXML2_VERSION}.tar.xz -o /tmp/libxml2-${LIBXML2_VERSION}.tar.xz
 
-# tar -C /tmp -xzf /tmp/libmcrypt-2.5.7.tar.gz
-# tar -C /tmp -xzf /tmp/cyrus-sasl-2.1.25.tar.gz
-tar -C /tmp -xzf /tmp/libmemcached-1.0.16.tar.gz
-tar -C /tmp -xzf /tmp/pcre-8.34.tar.gz
-tar -C /tmp -xzf /tmp/httpd-2.4.9.tar.gz
+echo "downloading ssl"
+curl -L https://github.com/openssl/openssl/archive/refs/tags/OpenSSL_${OPENSSL_VERSION}.tar.gz -o /tmp/OpenSSL_${OPENSSL_VERSION}.tar.gz
 
-tar -C /tmp/httpd-2.4.9/srclib -xzf /tmp/apr-1.5.0.tar.gz
-mv /tmp/httpd-2.4.9/srclib/apr-1.5.0 /tmp/httpd-2.4.9/srclib/apr
+echo "downloading curl"
+curl -L https://github.com/curl/curl/releases/download/curl-7_84_0/curl-${CURL_VERSION}.tar.gz -o /tmp/curl-${CURL_VERSION}.tar.gz
 
-tar -C /tmp/httpd-2.4.9/srclib -xzf /tmp/apr-util-1.5.3.tar.gz
-mv /tmp/httpd-2.4.9/srclib/apr-util-1.5.3 /tmp/httpd-2.4.9/srclib/apr-util
+tar -C /tmp -xzf /tmp/pcre-${PCRE_VERSION}.tar.gz
+tar -C /tmp -xzf /tmp/httpd-${HTTPD_VERSION}.tar.gz
 
-tar -C /tmp -xzf /tmp/php-5.5.11.tar.gz
-tar -C /tmp -xzf /tmp/memcached-2.1.0.tgz
-tar -C /tmp -xzf /tmp/zlib-1.2.8.tar.gz
-# tar -C /tmp -xzf /tmp/zip-1.10.2.tgz
+tar -C /tmp/httpd-${HTTPD_VERSION}/srclib -xzf /tmp/apr-${APR_VERSION}.tar.gz
+mv /tmp/httpd-${HTTPD_VERSION}/srclib/apr-${APR_VERSION} /tmp/httpd-${HTTPD_VERSION}/srclib/apr
+
+tar -C /tmp/httpd-${HTTPD_VERSION}/srclib -xzf /tmp/apr-util-${APR_UTIL_VERSION}.tar.gz
+mv /tmp/httpd-${HTTPD_VERSION}/srclib/apr-util-${APR_UTIL_VERSION} /tmp/httpd-${HTTPD_VERSION}/srclib/apr-util
+
+tar -C /tmp -xzf /tmp/php-${PHP_VERSION}.tar.gz
+tar -C /tmp -xzf /tmp/zlib-${ZLIB_VERSION}.tar.gz
+tar -C /tmp -xzf /tmp/expat-${EXPAT_VERSION}.tar.gz
+tar -C /tmp -xf /tmp/libxml2-${LIBXML2_VERSION}.tar.xz
+tar -C /tmp -xf /tmp/OpenSSL_${OPENSSL_VERSION}.tar.gz
+tar -C /tmp -xf /tmp/curl-${CURL_VERSION}.tar.gz
+
 
 export CFLAGS='-g0 -O2 -s -m64 -march=core2 -mtune=generic -pipe '
 export CXXFLAGS="${CFLAGS}"
 export CPPFLAGS="-I/app/local/include"
 export LD_LIBRARY_PATH="/app/local/lib"
-# export MAKEFLAGS="-j5"
-# export MAKE="/usr/bin/make $MAKEFLAGS"
 export MAKE="/usr/bin/make"
 
-# cd /tmp/libmcrypt-2.5.7
-# ./configure --prefix=/app/local --disable-posix-threads --enable-dynamic-loading --enable-static-link
-# ${MAKE} && ${MAKE} install
-
-cd /tmp/zlib-1.2.8
+cd /tmp/zlib-${ZLIB_VERSION}
 ./configure --prefix=/app/local --64
 ${MAKE} && ${MAKE} install
 
-cd /tmp/pcre-8.34
+cd /tmp/pcre-${PCRE_VERSION}
 ./configure --prefix=/app/local --enable-jit --enable-utf8
 ${MAKE} && ${MAKE} install
 
-cd /tmp/httpd-2.4.9
-./configure --prefix=/app/apache --enable-rewrite --enable-so --enable-deflate --enable-expires --enable-headers --enable-proxy-fcgi --with-mpm=event --with-included-apr --with-pcre=/app/local
+cd /tmp/expat-${EXPAT_VERSION}
+./configure --prefix=/app/local
+${MAKE} && ${MAKE} install
+
+cd /tmp/openssl-OpenSSL_${OPENSSL_VERSION}
+./config --prefix=/app/local --openssldir=/app/local
+${MAKE} depend
+${MAKE} all
+${MAKE} install
+
+cd /tmp/curl-${CURL_VERSION}
+./configure --with-openssl=/app/local/ --prefix=/app/local
+${MAKE} && ${MAKE} install
+
+cd /app/local/include
+ln -s /app/local/include/x86_64-linux-gnu/curl curl
+
+
+
+
+cd /tmp/libxml2-${LIBXML2_VERSION}
+./configure --prefix=/app/local
+${MAKE} && ${MAKE} install
+
+
+
+cd /tmp/httpd-${HTTPD_VERSION}
+./configure --prefix=/app/apache --enable-rewrite --enable-so --enable-deflate --enable-expires --enable-headers --enable-proxy-fcgi --with-mpm=event --with-included-apr --with-crypto --with-openssl=/app/local/openssl/lib --with-z=/app/local --with-expat=/app/local --with-pcre=/app/local/bin/pcre-config
 ${MAKE} && ${MAKE} install
 
 cd /tmp
-git clone git://github.com/ByteInternet/libapache-mod-fastcgi.git
+git clone https://github.com/ByteInternet/libapache-mod-fastcgi.git
 cd /tmp/libapache-mod-fastcgi/
 patch -p1 < debian/patches/byte-compile-against-apache24.diff 
 sed -e "s%/usr/local/apache2%/app/apache%" Makefile.AP2 > Makefile
 ${MAKE} && ${MAKE} install
 
-cd /tmp/php-5.5.11
-./configure --prefix=/app/php --with-pdo-pgsql --with-pgsql --with-mysql=mysqlnd --with-pdo-mysql=mysqlnd --with-iconv --with-gd --with-curl=/usr/lib --with-config-file-path=/app/php --enable-soap=shared --with-openssl --enable-mbstring --with-mhash --enable-mysqlnd --with-pear --with-mysqli=mysqlnd --with-jpeg-dir --with-png-dir --with-mcrypt=/app/local --enable-static --enable-fpm --with-pcre-dir=/app/local --disable-cgi --enable-zip
+cd /tmp/php-${PHP_VERSION}
+./configure --prefix=/app/php  --with-libxml-dir=/app/local --with-iconv --with-gd --with-curl=/app/local/ --with-config-file-path=/app/php --enable-soap=shared --enable-mbstring --with-mhash --with-pear --with-jpeg-dir --with-png-dir --with-mcrypt=/app/local --enable-static --enable-fpm --with-pcre-dir=/app/local --disable-cgi --enable-zip --with-openssl-dir=/app/local
 ${MAKE}
 ${MAKE} install
 
 /app/php/bin/pear config-set php_dir /app/php
-echo " " | /app/php/bin/pecl install memcache
-echo " " | /app/php/bin/pecl install apc-3.1.13
-echo " " | /app/php/bin/pecl install mongo
-/app/php/bin/pecl install igbinary
 
-# cd /tmp/cyrus-sasl-2.1.25
-# ./configure --prefix=/app/local
-# ${MAKE} && ${MAKE} install
-# export SASL_PATH=/app/local/lib/sasl2
-
-cd /tmp/libmemcached-1.0.16
-./configure --prefix=/app/local
-# the configure script detects sasl, but is still foobar'ed
-# sed -i 's/LIBMEMCACHED_WITH_SASL_SUPPORT 0/LIBMEMCACHED_WITH_SASL_SUPPORT 1/' Makefile
-${MAKE} && ${MAKE} install
-
-cd /tmp/memcached-2.1.0
-/app/php/bin/phpize
-./configure --with-libmemcached-dir=/app/local \
-  --prefix=/app/php \
-  --enable-memcached-igbinary \
-  --enable-memcached-json \
-  --with-php-config=/app/php/bin/php-config \
-  --enable-static
-${MAKE} && ${MAKE} install
-
-# cd /tmp/zip-1.10.2
-# /app/php/bin/phpize
-# ./configure --prefix=/app/php --with-php-config=/app/php/bin/php-config --enable-static
-# ${MAKE} && ${MAKE} install
-
-echo '2.4.9' > /app/apache/VERSION
-echo '5.5.11' > /app/php/VERSION
+echo '${HTTPD_VERSION}' > /app/apache/VERSION
+echo '${PHP_VERSION}' > /app/php/VERSION
 mkdir /tmp/build
 mkdir /tmp/build/local
 mkdir /tmp/build/local/lib
 mkdir /tmp/build/local/lib/sasl2
 cp -a /app/apache /tmp/build/
 cp -a /app/php /tmp/build/
-# cp -aL /usr/lib/libmysqlclient.so.16 /tmp/build/local/lib/
-# cp -aL /app/local/lib/libhashkit.so.2 /tmp/build/local/lib/
-cp -aL /app/local/lib/libmcrypt.so.4 /tmp/build/local/lib/
-cp -aL /app/local/lib/libmemcached.so.11 /tmp/build/local/lib/
-cp -aL /app/local/lib/libpcre.so.1 /tmp/build/local/lib/
-# cp -aL /app/local/lib/libmemcachedprotocol.so.0 /tmp/build/local/lib/
-# cp -aL /app/local/lib/libmemcachedutil.so.2 /tmp/build/local/lib/
-# cp -aL /app/local/lib/sasl2/*.so.2 /tmp/build/local/lib/sasl2/
+cp -aL /app/local/lib/libmcrypt.so.* /tmp/build/local/lib/
+cp -aL /app/local/lib/libpcre.so.* /tmp/build/local/lib/
 
 rm -rf /tmp/build/apache/manual/
+
 
